0fce7dd7af8eb4797da81cd7ff4f51af7ee46629
cokvrindaa <master.cokorda@gmail.com>
2026-02-19 17:47:14 +0800

    switchCase

--- CPP TI/basic/if else/latihanSwitchCase.cpp
+++ CPP TI/basic/if else/latihanSwitchCase.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 int main(){
     int metodePembayaran;
+
     cout << "1. Cash" << endl << "2. Transfer" << endl << "3. Qriss" << endl;
     cout << "Silahkan masukan metode pembayaran anda: " ;
     cin >> metodePembayaran;
     switch (metodePembayaran){
         case 1:
-            cout << "Cash";
+            cout << "Anda memilih Cash!";
             break;
         case 2:
-            cout << "Transfer";
+            cout << "Anda memilih Transfer!";
             break;
         case 3:
-            cout << "Qriss";
+            cout << "Anda memilih Qriss!";
             break;
         default:
-            cout << "Kode pembayaran tidak tersedia";
+            cout << "Kode pembayaran invalid, pilih sesuai nomor yang disediakan";
     }
+    return 0;
 }
